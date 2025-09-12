cf21539cb86ef1d79b7dc9c3af1177707376a8c1
emil <emst@eadania.dk>
2025-09-12 09:26:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace basisprogrammering_operatorors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number:");
+
+            float a = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            float b = float.Parse(Console.ReadLine());
+            Console.WriteLine("results of maths:");
+            Console.WriteLine($"a + b =  {a + b}");
+            Console.WriteLine($"a - b =  {a - b}");
+            Console.WriteLine($"a * b =  {a * b}");
+            Console.WriteLine($"a / b =  {a / b}");
+            Console.WriteLine($"(a + b)*2 =  {(a+b)*2}");
+            Console.WriteLine("press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
